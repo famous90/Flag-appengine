84169a235cfea56ab9f2d7fcfb4b5c649246070f
이 우석 <Will-Mac@iui-MacBook-Pro-2.local>
2014-01-27 17:47:28 +0900

    no message

--- src/com/anb/flag/engine/utils/LocationUtils.java
+++ src/com/anb/flag/engine/utils/LocationUtils.java
@@ -2,19 +2,31 @@ package com.anb.flag.engine.utils;
 
 public class LocationUtils {
 	public static final double NEAR_DISTANCE_DEGREE = 0.0089524654;
+	public static final double CLOSE_DISTANCE_DEGREE = 0.0044762327;
+	public static final double AT_DISTANCE_DEGREE = 0.0022381163;
 
-	public static boolean isNearby(double latx, double lonx, double laty, double lony) {
+	public static boolean isNear(double latx, double lonx, double laty, double lony) {
 		if (distance(latx, lonx, laty, lony) < NEAR_DISTANCE_DEGREE)
 			return true;
 		else
 			return false;
 	}
 
+	public static boolean isClose(double latx, double lonx, double laty, double lony) {
+		if (distance(latx, lonx, laty, lony) < CLOSE_DISTANCE_DEGREE)
+			return true;
+		else
+			return false;
+	}
+
+	public static boolean isAt(double latx, double lonx, double laty, double lony) {
+		if (distance(latx, lonx, laty, lony) < AT_DISTANCE_DEGREE)
+			return true;
+		else
+			return false;
+	}
+
 	public static double distance(double latx, double lonx, double laty, double lony) {
-		double distance = 0;
-
-		distance = Math.sqrt((latx - laty) * (latx - laty) + (lonx - lony) * (lonx - lony));
-
-		return distance;
+		return Math.sqrt((latx - laty) * (latx - laty) + (lonx - lony) * (lonx - lony));
 	}
 }
